279dee77e18841c150974b0229dab966dbd38957
miitoe <milla.k.maatta@gmail.com>
2020-10-23 15:00:56 +0300

    aniscript
    aniscript

---
+++ SIENIPATARUMPU/Assets/Scripts/AnimControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+    public GameObject theNPC;
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetButtonDown("GKey"))
+        {
+            theNPC.GetComponent<Animator>().Play("Pallo_Y_tap01");
+        }
+    }
+}
