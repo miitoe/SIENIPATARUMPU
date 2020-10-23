4f35616902eee9653761163129269fc088ca4a13
miitoe <milla.k.maatta@gmail.com>
2020-10-23 15:46:58 +0300

    Audio script
    Audio script

--- SIENIPATARUMPU/Assets/Scripts/Particle_keypress.cs
+++ SIENIPATARUMPU/Assets/Scripts/Particle_keypress.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyG_press : MonoBehaviour
+public class Particle_keypress : MonoBehaviour
 {
     // Start is called before the first frame update
     // void Start()
